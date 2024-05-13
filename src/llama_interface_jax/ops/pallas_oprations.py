0e016b311ee92f8f3ea9acb7d6f9df1f55ff62c6
erfanzar <erfanzare82@yahoo.com>
2024-05-13 18:35:14 +0330

    fixing pallas matmul issues and adding flash attention

--- src/llama_interface_jax/ops/pallas_oprations.py
+++ src/llama_interface_jax/ops/pallas_oprations.py
@@ -1,24 +1,23 @@
 # A simple fused matmul operation for pallas
-
-import os
-
-os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 import jax
 from functools import partial
-
 from jax.experimental import pallas as pl
-from jax import numpy as jnp, ShapeDtypeStruct, Array, jit
+from jax import numpy as jnp, ShapeDtypeStruct, Array, jit, lax
 
 DEFAULT_SUB_LINE = 4
+FIXED_MATMUL_DIM = 2
 
 
 def _matmul_2d_kernel_u_ref(lhs_ref, rhs_ref, o_ref, *, block_size):
-    for k in range(lhs_ref.shape[1] // block_size):
-        o_ref[:, :] += lhs_ref[:, k * block_size:(k + 1) * block_size] @ rhs_ref[k * block_size:(k + 1) * block_size, :]
+    for idx in range(lhs_ref.shape[1] // block_size):
+        o_ref[:, :] += lhs_ref[
+                       :, idx * block_size:(idx + 1) * block_size
+                       ] @ rhs_ref[idx * block_size:(idx + 1) * block_size, :
+                           ]
 
 
 @partial(jit, static_argnames=["block_sizes"])
-def matmul_2d_u_ref(lhs: Array, rhs: Array, block_sizes: tuple[int, ...] | None = None) -> Array:
+def _matmul_2d_u_ref(lhs: Array, rhs: Array, block_sizes: tuple[int, ...] | None = None) -> Array:
     assert lhs.ndim == rhs.ndim, "lhs and rhs must have same ndim"
 
     if block_sizes is None:
@@ -42,17 +41,69 @@ def matmul_2d_u_ref(lhs: Array, rhs: Array, block_sizes: tuple[int, ...] | None 
     )(lhs, rhs)
 
 
+def _matmul_kernel(x_ref, y_ref, z_ref, *, precision):
+    z_ref[...] = jnp.dot(x_ref[...], y_ref[...], precision=precision)
+
+
+@partial(jit, static_argnames=["interpret", "precision", "block_size"])
+def _matmul_2d(
+        lhs: Array,
+        rhs: Array,
+        block_size: int | None = None,
+        precision: lax.PrecisionLike = None,
+        interpret: bool = False
+) -> Array:
+    assert lhs.ndim == rhs.ndim, "lhs and rhs must have same ndim"
+    if block_size is None:
+        block_rhs = pl.cdiv(rhs.shape[1], 16)
+        block_lhs = pl.cdiv(lhs.shape[0], 16)
+    else:
+        block_rhs = block_size
+        block_lhs = block_size
+    return pl.pallas_call(
+        partial(_matmul_kernel, precision=precision),
+        out_shape=jax.ShapeDtypeStruct((lhs.shape[0], rhs.shape[1]), lhs.dtype),
+        grid=(block_lhs, block_rhs),
+        in_specs=[
+            pl.BlockSpec(lambda i, j: (i, 0), (lhs.shape[0] // block_lhs, lhs.shape[1])),
+            pl.BlockSpec(lambda i, j: (0, j), (rhs.shape[0], rhs.shape[1] // block_rhs))
+        ],
+        out_specs=pl.BlockSpec(
+            lambda i, j: (i, j), (lhs.shape[0] // block_lhs, rhs.shape[1] // block_rhs)
+        ),
+        interpret=interpret,
+    )(lhs, rhs)
+
+
+@partial(jit, static_argnames=["interpret", "precision", "block_size"])
+def matmul(
+        lhs: Array,
+        rhs: Array,
+        block_size: int | None = None,
+        precision: lax.PrecisionLike = None,
+        interpret: bool = False
+):
+    func = partial(_matmul_2d, block_size=block_size, precision=precision, interpret=interpret)
+    assert lhs.ndim == rhs.ndim, "lhs and rhs must have same ndim"
+    assert lhs.ndim >= 2, "Only 1d arrays are not supported."
+    tdim = lhs.ndim
+    if tdim > FIXED_MATMUL_DIM:
+        for _ in range(tdim - FIXED_MATMUL_DIM):
+            func = jax.vmap(func)
+    return func(lhs, rhs)
+
+
 if __name__ == "__main__":
     batch_size = 8
-    seq_len = 256
+    seq_len = 64
     num_heads = 32
     head_dims = 128
-    # x = jax.random.normal(jax.random.key(0), (seq_len, head_dims), dtype=jnp.float32)
-    # y = jax.random.normal(jax.random.key(1), (head_dims, seq_len), dtype=jnp.float32)
-    x = jnp.ones((512, 256), dtype=jnp.float32)
-    y = jnp.ones((256, 1024), dtype=jnp.float32)
-    res = matmul_2d_u_ref(x, y)
-    org = x @ y
+    k1, k2, k3 = jax.random.split(jax.random.key(0), num=3)
+    q = jax.random.normal(k1, (batch_size, seq_len, num_heads, head_dims))
+    k = jax.random.normal(k2, (batch_size, seq_len, num_heads, head_dims))
+
+    res = matmul(q, k, block_size=64)
+    org = q @ k
 
     org_sum = jnp.mean(jnp.sum(org, axis=0))
     res_sum = jnp.mean(jnp.sum(res, axis=0))
@@ -60,4 +111,4 @@ if __name__ == "__main__":
     print(org_sum)
     print(res_sum)
 
-    print(jnp.allclose(res, res))
+    print(jnp.allclose(res, org))
